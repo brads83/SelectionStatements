cae40256073375bcca16ed164b64f17db1e67b57
brads83 <bradsorrells73@gmail.com>
2020-04-06 08:48:13 -0500

    started exercise

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,7 +6,25 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a number between 0-100 to guess my favorite number");
+            var favNumber = 12;
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too low, try again");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High, try again");
+                Console.ReadLine();
+            }
+            else (userInput < favNumber)
+                { 
+                       
+                }
+            
+            
+            
         }
     }
 }
