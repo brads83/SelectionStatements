e7f3b09b58aa9818f567c79d5cf23706dca828ec
brads83 <bradsorrells73@gmail.com>
2020-04-06 14:04:53 -0500

    updated with do-while

--- SelectionStatements/Program.cs
+++ SelectionStatements/Program.cs
@@ -6,25 +6,31 @@ namespace SelectionStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number between 0-100 to guess my favorite number");
+            
             var favNumber = 12;
-            var userInput = int.Parse(Console.ReadLine());
-            if (userInput < favNumber)
+            int userInput;
+            
+            do
             {
-                Console.WriteLine("Too low, try again");
-            }
-            else if (userInput > favNumber)
-            {
-                Console.WriteLine("Too High, try again");
-                Console.ReadLine();
-            }
-            else (userInput == favNumber)
+                Console.WriteLine("Enter a number between 0-100 to guess my favorite number");
+                userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
                 {
-                Console.WriteLine("Bingo, you got it!");     
+                    Console.WriteLine("Too low, try again");
+                    
                 }
-            
-            
-            
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Too High, try again");
+                    
+                }
+                else 
+                {
+                    Console.WriteLine("Bingo, you got it!");
+                } 
+            } while (userInput != favNumber);
+
         }
     }
 }
